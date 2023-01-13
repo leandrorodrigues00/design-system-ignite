22f98e9b427dd54905c9b399bd42f050f57bdd81
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2023-01-13 10:28:08 -0300

    fix: Renaming packages for NPM publishing

--- packages/docs/src/stories/TextInput.stories.tsx
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -1,5 +1,10 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { Box, Text, TextInput, TextInputProps } from '@ignite-ui/react'
+import {
+  Box,
+  Text,
+  TextInput,
+  TextInputProps,
+} from '@ignis-design-system/react'
 
 export default {
   title: 'Form/Text Input',
